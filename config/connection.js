a086e679d897f8c42ad75ce71aacb3ebdcbd3d0a
Frank Mcdonald <frankmcd@cox.net>
2017-07-28 16:42:13 -0700

    added content to js files

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "CA-stle2134",
-    database: "burger_db"
+    database: "burgers_db"
 });
 
 connection.connect(function(err) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,4 @@ INSERT INTO burgers(burger_name) VALUES ('Bacon Cheeseburger');
 INSERT INTO burgers(burger_name) VALUES ('Double Double Burger');
 INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
 INSERT INTO burgers(burger_name) VALUES ('Black & Bleu Burger');
+INSERT INTO burgers(burger_name) VALUES ('Krabby Patty');
